8d14908e477a1b04c45ea537cea1ea2c2c0386f6
Alex Hollenbeck <alexhollenbeck@gmail.com>
2024-02-24 17:49:36 -0600

    chore: netlify plugin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,4 +22,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Alex Hollenbeck | Full-stack web developer</title>
